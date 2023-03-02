bbe8efa10fae1f6266a5a4d8fecf1839ce8c8ae4
Juan Bernal Martinez <Juan Bernal Martinez@DESKTOP-Q8G3AKF>
2023-03-02 11:37:47 -0600

    Turkey

--- DuckToTurkey/Class1.cs
+++ DuckToTurkey/Class1.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace DuckToTurkey
 {
-    class Class1
+    abstract class Pato
     {
+        private readonly IGraznar _estrategiaGraznar;
+        private readonly IVolar _estrategiaVolar;
+
+        protected Pato(IGraznar estrategiaGraznar, IVolar estrategiaVolar)
+        {
+            _estrategiaGraznar = estrategiaGraznar;
+            _estrategiaVolar = estrategiaVolar;
+        }
+
+        public void Graznar()
+        {
+            _estrategiaGraznar.Graznar();
+        }
+
+        public void Volar()
+        {
+            _estrategiaVolar.Volar();
+        }
+
+        public void Nadar()
+        {
+            Console.WriteLine("Nadando");
+        }
+
+        public void ShowDuck()
+        {
+            Mostrar();
+            Graznar();
+            Volar();
+            Nadar();
+        }
+
+
+        public abstract void Mostrar();
     }
 }
--- DuckToTurkey/Class2.cs
+++ DuckToTurkey/Class2.cs
@@ -6,7 +6,75 @@ using System.Threading.Tasks;
 
 namespace DuckToTurkey
 {
-    class Class2
+    public class VolarConAlas : IVolar
     {
+        public void Volar()
+        {
+            Console.WriteLine("Volando con alas");
+        }
+    }
+
+    public class NoVolar : IVolar
+    {
+        public void Volar()
+        {
+            Console.WriteLine("No puedo volar");
+        }
+    }
+    class Mallard : Pato
+    {
+        public Mallard() :
+            base(new Quack(), new VolarConAlas())
+        {
+        }
+
+        public override void Mostrar()
+        {
+            Console.WriteLine("Soy un Mallard");
+        }
+    }
+
+    class Rubber : Pato
+    {
+        public Rubber() :
+                base(new Squeze(), new NoVolar())
+        {
+        }
+
+
+        public override void Mostrar()
+        {
+            Console.WriteLine("Soy un Rubber");
+        }
+    }
+
+    class DuckDecoy : Pato
+    {
+        public DuckDecoy() :
+            base(new Mute(), new NoVolar())
+        {
+
+        }
+
+
+        public override void Mostrar()
+        {
+            Console.WriteLine("Soy un Decoy");
+        }
+    }
+
+    class RedHead : Pato
+    {
+        public RedHead() :
+            base(new Quack(), new VolarConAlas())
+        {
+
+        }
+
+
+        public override void Mostrar()
+        {
+            Console.WriteLine("Soy un RedHead");
+        }
     }
 }
